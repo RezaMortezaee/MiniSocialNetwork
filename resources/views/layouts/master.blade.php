757ae26f7dfedf895d5301f9ac4ddaf6be5f14e5
RezaMrtz <reza.mrtz@gmail.com>
2017-12-16 23:47:29 +0330

    SignUp and Login View

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @include('includes.header')
+        <div class="container">
+            @yield('content')            
+        </div>
+    </body>
+</html>
